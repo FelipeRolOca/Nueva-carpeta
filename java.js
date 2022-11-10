587d554ee8a8a843f32ca26296c4ca1a577fa575
FelipeRolOca <felipeoca123@hotmail.com>
2022-11-10 18:11:24 -0300

    add

--- java.js
+++ java.js
@@ -6,18 +6,41 @@ var total = 0;
 peticion.addEventListener("readystatechange", function () {
     if (this.readyState == 4 && this.status == 200) {
         var productos = JSON.parse(this.responseText);
+        var total = 0
        
         productos.forEach(p => {
-            
+            var div = document.querySelector ('.productos')
+            var casillas = document.createElement ('div')
+            div.appendChild (casillas) ;
             var link = document.createElement('a');
             link.setAttribute('href', '#');
             link.innerText = p.nombre;
+            var imagen = document.createElement ('img');
+            imagen.setAttribute ('alt', "imagen del producto");
+            imagen.setAttribute ('src', p.url_foto);
+            var description = document.createElement ('p');
+            description.innerText = p.descripcion;
+            var precio = document.createElement ('p');
+            precio.innerText = p.precio;
+            casillas.appendChild (link)
+            casillas.appendChild (imagen)
+            casillas.appendChild (description)
+            casillas.appendChild (precio)
+            link.addEventListener ('click', function () {
+                var contador = 0;
+                contador = contador + 1;
+                var lugardelcontador = document.querySelector ("#contador");
+                lugardelcontador.innerText = contador;
+            })
 
            
             link.addEventListener('click', function () {
                 console.log(p.nombre, p.precio);
-                total = total + p.precio;
+                 total = total + p.precio;
                 console.log("total: ", total);
+                var preciofinal = document.querySelector("#total");
+                preciofinal.innerText = total;
+                
 
                 
                 var fila = document.createElement('tr');
@@ -42,12 +65,17 @@ peticion.addEventListener("readystatechange", function () {
                     console.log(event.target.parentElement.parentElement.remove());
                     total = total - p.precio;
                     console.log("total: ", total);
+                  
+                    
                 });
+                var precioalto = -9 ;
+                if (p.precio > precioalto) {
+                    precioalto = p.precio                   
+                }
+                console.log (precioalto)
 
                 document.querySelector('tbody').appendChild(fila);
             })
-
-            document.querySelector('.productos').appendChild(link);
         });
     }
 });
